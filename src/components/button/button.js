cdbff4264d69e7133ca40827c8a46295b21ea679
filipemarins <lipe_bm@yahoo.com.br>
2020-02-16 16:10:55 -0300

    Add button component

---
+++ src/components/button/button.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import { space } from 'styled-system';
+
+import MaterialButton from '@material-ui/core/Button';
+
+const Button = styled(MaterialButton)`
+  ${space}
+`;
+
+export default Button;
