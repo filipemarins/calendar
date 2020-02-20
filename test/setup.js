3cf51f4133e693f812ade8f05ed95b62b4134e9a
filipemarins <lipe_bm@yahoo.com.br>
2020-02-20 01:21:59 -0300

    Add next router jest mock

--- test/setup.js
+++ test/setup.js
@@ -2,3 +2,11 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
+
+jest.mock('next/router', () => ({
+  useRouter() {
+    return {
+      pathname: '/mockpath',
+    };
+  },
+}));
